c3980993e991c128daaa8b967ace41f85b355bbf
ifuensan <ibiko1@gmail.com>
2024-09-09 08:26:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-#
-apt update
+# Update the apt repositories
+sudo apt update
+
+# Navigate to the directory where the docker-compose.yml file is located
 cd code/docker
+
+# Start Docker services in detached mode (background)
 docker compose up -d
+
+# Print a separation or status message
+echo "======================================================"
